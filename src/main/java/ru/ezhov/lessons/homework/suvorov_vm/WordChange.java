9740f858b639e795ba3eb13852a4b519f5001b4c
Utachka <pronubos@mail.ru>
2017-08-01 00:39:46 +0300

    first commit

--- src/main/java/ru/ezhov/lessons/homework/suvorov_vm/WordChange.java
+++ src/main/java/ru/ezhov/lessons/homework/suvorov_vm/WordChange.java
@@ -1,7 +1,15 @@
 package ru.ezhov.lessons.homework.suvorov_vm;
 
+import java.util.Scanner;
+
 public class WordChange {
     public static void main(String[] args) {
-        System.out.println("Перевод");
+        System.out.println("Введите текст");
+        Scanner scan = new Scanner(System.in);
+        String name = scan.next();
+
+
     }
 }
+
+public class Vichislenie
