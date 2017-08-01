6ce26aa4d7f3e78dac0c693987d8d2cee76b8a27
Utachka <pronubos@mail.ru>
2017-08-01 23:44:51 +0300

    дописал прогу , использовал 1 метод мейн

--- src/main/java/ru/ezhov/lessons/homework/suvorov_vm/WordChange.java
+++ src/main/java/ru/ezhov/lessons/homework/suvorov_vm/WordChange.java
@@ -8,8 +8,18 @@ public class WordChange {
         Scanner scan = new Scanner(System.in);
         String name = scan.next();
 
+        char[] strName = name.toCharArray();
 
+        char firstSimbol = strName[0];
+        char endSimbol = strName[strName.length - 1];
+        String seredinaSlova ="";
+
+        for(int i = strName.length - 2; i > 0; i--)
+        {
+            seredinaSlova = seredinaSlova + strName[i];
+        }
+
+        System.out.println(firstSimbol + seredinaSlova + endSimbol);
     }
 }
 
-public class Vichislenie
