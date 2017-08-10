bedacb47142cb11b49edae3836f77d976e6e0e82
ezhov-da <ezhdenis@yandex.ru>
2017-08-10 17:09:11 +0300

    changed some files

--- src/main/java/ru/ezhov/lessons/homework/suvorov_vm/Glava1/Var2_1.java
+++ src/main/java/ru/ezhov/lessons/homework/suvorov_vm/Glava1/Var2_1.java
@@ -1,7 +1,9 @@
 package ru.ezhov.lessons.homework.suvorov_vm.Glava1;
+
 import java.util.Scanner;
+
 class Var2_1 {
-    public static void main (String[] args){
+    public static void main(String[] args) {
         System.out.println("Введите через пробел целые числа");
         Scanner scan = new Scanner(System.in);
         String valStr = scan.nextLine();
@@ -9,19 +11,17 @@ class Var2_1 {
         viborOperation(massValStr);
     }
 
-    public static void viborOperation(String[] val)
-    {
-        try
-        {
+    public static void viborOperation(String[] val) {
+        try {
             int[] intArrVal = arrInt(val);
             System.out.println("Выберите пункт для действий " +
                     "\n1..... " +
                     "\n2.....");
             Scanner scan = new Scanner(System.in);
             String vibrannyPunct = scan.next();
-            try{
+            try {
                 int vibor = Integer.parseInt(vibrannyPunct);
-                switch(vibor) {
+                switch (vibor) {
                     case 1:
                         chetn(intArrVal);
                         break;
@@ -31,47 +31,35 @@ class Var2_1 {
                     default:
                         System.out.println("Такой функции еще не добавлено");
                 }
-            }
-            catch(Exception e)
-            {
+            } catch (Exception e) {
                 System.out.println("Ввести нужно только одну цифру");
             }
-        }
-        catch(Exception e)
-        {
+        } catch (Exception e) {
             System.out.println("Ввели не корректные данные");
         }
     }
 
-    public static int[] arrInt(String[] val)
-    {
+    public static int[] arrInt(String[] val) {
         int[] valInt = new int[val.length];
-        for(int i = 0; i < val.length; i++)
-        {
+        for (int i = 0; i < val.length; i++) {
             valInt[i] = Integer.parseInt(val[i]);
         }
         return valInt;
     }
 
-    public static void chetn(int[] val)
-    {
-        for(int i = 0; i < val.length ; i++)
-        {
+    public static void chetn(int[] val) {
+        for (int i = 0; i < val.length; i++) {
 
-            if((val[i] % 2) ==0)
-            {
+            if ((val[i] % 2) == 0) {
                 System.out.print(val[i] + " ");
             }
 
         }
     }
 
-    public static void nechetn(int[] val)
-    {
-        for(int i = 0; i < val.length; i++)
-        {
-            if((val[i] % 2) != 0)
-            {
+    public static void nechetn(int[] val) {
+        for (int i = 0; i < val.length; i++) {
+            if ((val[i] % 2) != 0) {
                 System.out.print(val[i] + " ");
             }
 
