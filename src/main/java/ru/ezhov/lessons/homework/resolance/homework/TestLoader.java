a4b1c8f725dfe68212883750eef52105009cd227
resolance <kapusta>
2017-09-07 08:26:48 +0300

    +Interface

---
+++ src/main/java/ru/ezhov/lessons/homework/resolance/homework/TestLoader.java
@@ -0,0 +1,8 @@
+package ru.ezhov.lessons.homework.resolance.homework;
+
+public class TestLoader {
+    public static void main(String[] args) {
+        UseLoader userL =  new UseLoader();
+        userL.load("Test loader");
+    }
+}
