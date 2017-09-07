21f71635c4fc6cd4515bf665baab9bf18e8fd574
resolance <kapusta>
2017-09-07 08:11:57 +0300

    +Interface

--- src/main/java/ru/ezhov/lessons/homework/resolance/homework/UseLoader.java
+++ src/main/java/ru/ezhov/lessons/homework/resolance/homework/UseLoader.java
@@ -1,11 +1,16 @@
 package ru.ezhov.lessons.homework.resolance.homework;
 
-public class UseLoader {
-    private final Loader loader;
+public class UseLoader implements Loader {
+    //private final Loader loader;
 
-    public UseLoader(final Loader loader) {
-        this.loader = loader;
+    @Override
+    public void load(String string) {
+        this.load("Check load");
     }
+
+    /*public UseLoader(final Loader loader) {
+        this.loader = loader;
+    }*/
 
     /*public void UseLoader(final String string){
         loader.load(string);
